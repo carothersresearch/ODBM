a3ac61861f38cc2a4b712332103916dd43ff7026
cardiffryan <cardiffr@uw.edu>
2021-11-01 17:22:16 -0700

    added "writeReactions" function

--- buildModelFromExcel.py
+++ buildModelFromExcel.py
@@ -24,6 +24,37 @@ def initializeValues(model_species, model_rxns):
     with open('model.txt', 'w') as f:
         f.write(model_str)
 
-
+#add support so either function can be called first - right now initializeValues must be called first, then writeReactions
+#maybe make a more intuitive way of having non 1:1 stoichiometries - right now if A -> 2B, excel wants B, B in "Product" column
 def writeReactions():
+    rxn_str = '\n #Define reactions \n'
+    for rxn in model_rxns.iterrows():
+        if rxn[1]['Mechanism'] == 'MA':
+            if str(rxn[1]['Enzyme']) != 'nan':
+                # mass action kinetics
+                for r in rxn[1]['Reactant'].split(','):
+                    rxn_str += r + ' +'
+                rxn_str = rxn_str[:-2] + ' + ' + rxn[1]['Enzyme'] + ' -> '
+                for p in rxn[1]['Product'].split(','):
+                    rxn_str += p + ' +'
+                rxn_str = rxn_str[:-2] + ' + ' + rxn[1]['Enzyme'] + '; '   
+                rxn_str += 'K1_' + rxn[1]['Label']
+                for p in rxn[1]['Product'].split(','):
+                    rxn_str += '*' + p 
+                rxn_str += ' \n'
+            else:
+                for r in rxn[1]['Reactant'].split(','):
+                    rxn_str += r + ' +'
+                rxn_str = rxn_str[:-2] + ' -> '
+                for p in rxn[1]['Product'].split(','):
+                    rxn_str += p + ' +'
+                rxn_str = rxn_str[:-2] + '; '
+                rxn_str += 'K1_' + rxn[1]['Label']
+                for p in rxn[1]['Product'].split(','):
+                    rxn_str += '*' + p 
+                rxn_str += ' \n'
+    with open('model.txt', 'a') as f:
+    f.write(rxn_str)
     
+
+                

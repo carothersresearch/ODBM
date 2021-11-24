8b380a5f5925c9291489a3083d4f2a185f128ad6
DIEGOA363 <dalba@uw.edu>
2021-11-23 17:16:31 -0800

    ModelHandler

--- odbm/tests/test_odbm.py
+++ odbm/tests/test_odbm.py
@@ -3,8 +3,30 @@ import unittest
 from odbm.odbm import *
 
 
-class TestModelBuilder(unittest.TestCase):
+class Test_ModelBuilder(unittest.TestCase):
+
+    def test_update(sef): # modify internal species and rxns and recompile?
+        pass
+
     pass
 
-class TestModelHandler(unittest.TestCase):
+class Test_ModelHandler(unittest.TestCase):
+    def test__init__(self):
+        pass
+
+    def test_setBoundarySpecies(self):
+        pass
+
+    def test_setConcentrations(self):
+        pass
+
+    def test_senstivityAnalysis(self):
+        pass
+
+    def test_setSimulationConditions(self):
+        pass
+    
+    def test_run(self):
+        pass
+
     pass

8d0cbc2ff6f43fdceff56212a05e126aaa0ef6d5
DIEGOA363 <dalba@uw.edu>
2021-11-23 17:07:40 -0800

    vis update, test folder

---
+++ odbm/tests/test_odbm.py
@@ -0,0 +1,10 @@
+"""Unit tests for ODBM module"""
+import unittest
+from odbm.odbm import *
+
+
+class TestModelBuilder(unittest.TestCase):
+    pass
+
+class TestModelHandler(unittest.TestCase):
+    pass

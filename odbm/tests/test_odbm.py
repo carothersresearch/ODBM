5796cd04e885adf40820bbd0acb39a2df56b3095
cardiffryan <cardiffr@uw.edu>
2021-11-29 13:16:12 -0800

    began unit tests for odbm

--- odbm/tests/test_odbm.py
+++ odbm/tests/test_odbm.py
@@ -1,18 +1,46 @@
 """Unit tests for ODBM module"""
 import unittest
+import numpy as np
 from odbm.odbm import *
 
 
 class Test_ModelBuilder(unittest.TestCase):
+    """Unit test for ModelBuilder parent class"""
+    def test___init___(self):
+        #Test proper initialization of class with proper inputs
+        subject = ModelBuilder(species=np.array(['A', 'B', 'C', 'D']),
+                                 rxn=np.array(['R1', 'R2', 'R3']),
+                                 mech_dict={})
+    
+        # check to make sure necessary attributes are inputted
+        self.assertTrue(subject.species is not None)
+        self.assertTrue(subject.species is not None)
+        self.assertFalse(subject.mech_dict)
+    
+    def test_addMechanism(self):
+        # test to see if mech_dict dictionary updates with addMechanism
+        m = MichaelisMenten
+        subject.addMechanism(m)
+        self.assertTrue(subject.mech_dict)
 
-    def test_update(sef): # modify internal species and rxns and recompile?
-        pass
+    def test_addSpecies(self):
+        Label = 'E' 
+        StartingConc = '0'
+        subject.addSpecies(Label, StartingConc)
+        self.assertTrue(subject.species['Label'].str.contains(Label).any())
 
-    pass
+    def test_addReaction(self):
+        mech = 'MA'
+        sub = 'A,B'
+        prod = 'C'
+        param = 'k:100'
+        label = 'R4'
+        subject.addReaction(Mechanism = mech, Substrate = sub, Product = prod, Parameters = param, Label = label)
+        self.assertTrue(subject.rxns['Label'].str.contains(Label).any()))
 
-class Test_ModelHandler(unittest.TestCase):
-    def test__init__(self):
-        pass
+    def test_writeReaction(self):
+        test_str = subject.writeReaction(subject.rxns[subject.rxns['Label']=='R4'])
+        # compare what this returns to what we expect? Should we do this for each mechanism type?
 
     def test_setBoundarySpecies(self):
         pass

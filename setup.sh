fa8c40e8c091e5af7149a37f99f581c4942b7faa
DIEGOA363 <dalba@uw.edu>
2023-09-20 15:29:39 -0700

    update setup

--- setup.sh
+++ setup.sh
@@ -4,12 +4,10 @@ echo " "
 echo "making 'ODBM' conda virtual environment..."
 conda create -n ODBM python=3.6
 activate ODBM
-pip install tellurium
-pip install pandas
-pip install xlrd
-pip install openpyxl
-pip install overrides
-pip install notebook
+pip install tellurium==2.1.5
+pip install xlrd==1.2.0
+pip install openpyxl==3.0.0
+pip install notebook==6.3.0
 
 echo " "
 
